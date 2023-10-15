af446043bd914a54a13bd381fa3a6ad71190403e
Bob Bass <bob@Bobs-MacBook-Air-M2.local>
2023-10-14 19:18:57 -0500

    some nonsense to get it to stop complaining

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ func main() {
 	cards := newDeck()
 	cards.saveToFile(FILEPATH)
 
+	hand, cards := deal(cards, 5)
+	cards.print()
+	hand.print()
+
 	cards = newDeckFromFile(FILEPATH)
 	cards.shuffle()
 	cards.print()
